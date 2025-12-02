18e54a0a3011a676cc74880778784676fac8b3ad
SoundDrill31 <84176052+sounddrill31@users.noreply.github.com>
2025-12-02 14:16:00 +0530

    Try to use COOL_TOPSRCDIR to override srcdir

--- make-appimage.sh
+++ make-appimage.sh
@@ -10,10 +10,10 @@ export ARCH VERSION
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.bg.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
-export ICON="https://raw.githubusercontent.com/CollaboraOnline/online/refs/heads/distro/collabora/coda-$VERSION/windows/coda/Assets/logo.png"
+export ICON="https://raw.githubusercontent.com/CollaboraOnline/online/refs/heads/distro/collabora/coda-$VERSION/qt/hicolor/scalable/apps/com.collabora.Office.startcenter.svg"
 export OUTNAME=CODA-"$VERSION"-anylinux-"$ARCH".AppImage
 export ANYLINUX_LIB=1
-#export DESKTOP=PATH_OR_URL_TO_DESKTOP_ENTRY
+export DESKTOP="https://raw.githubusercontent.com/CollaboraOnline/online/refs/heads/distro/collabora/coda-25.04/qt/com.collabora.Office.desktop"
 export OPTIMIZE_LAUNCH=1
 
 # Deploy dependencies
@@ -21,23 +21,27 @@ quick-sharun /usr/bin/coda-qt \
     /usr/share/coda-qt \
     /usr/share/coolwsd
 
-cp -r "$CODA_PATH"/browser ./AppDir
+#cp -r "$CODA_PATH"/browser ./AppDir
 cp -r "$CODA_PATH"/core ./AppDir
 
+#echo '#!/bin/sh
+#mkdir -p /tmp/CODA
+#ln -sfn "$APPDIR"/browser /tmp/CODA/browser
+#ln -sfn "$APPDIR"/core /tmp/CODA/core
+#' > ./AppDir/bin/fix-bruhmoment.hook
+
 echo '#!/bin/sh
-mkdir -p /tmp/CODA
-ln -sfn "$APPDIR"/browser /tmp/CODA/browser
-ln -sfn "$APPDIR"/core /tmp/CODA/core
-' > ./AppDir/bin/fix-bruhmoment.hook
+export COOL_TOPSRCDIR="$(pwd)/share/coolwsd"
+' > ./AppDir/bin/override-topsrcdir.hook
 
 chmod +x ./AppDir/bin/fix-bruhmoment.hook
 
 # debloat
 rm -rf \
-    ./AppDir/core/include         \
-    ./AppDir/browser/po           \
-    ./AppDir/browser/node_modules \
-    ./AppDir/browser/archived-packages
+    ./AppDir/core/include #        \
+#    ./AppDir/browser/po           \
+#    ./AppDir/browser/node_modules \
+#    ./AppDir/browser/archived-packages
 
 # Turn AppDir into AppImage
 quick-sharun --make-appimage
