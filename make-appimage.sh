fbfe8e0d10e2ec39c394b4d1923fe682980ca2c3
SoundDrill31 <84176052+sounddrill31@users.noreply.github.com>
2025-12-09 12:52:53 +0530

    Update meta, desktop and icon

--- make-appimage.sh
+++ make-appimage.sh
@@ -10,9 +10,9 @@ export ARCH VERSION
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.bg.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
-export ICON="https://raw.githubusercontent.com/CollaboraOnline/online/refs/heads/distro/collabora/coda-$VERSION/windows/coda/Assets/logo.png"
-export DESKTOP="https://raw.githubusercontent.com/CollaboraOnline/online/refs/heads/distro/collabora/coda-$VERSION/qt/com.collabora.Office.desktop"
-export META="https://raw.githubusercontent.com/CollaboraOnline/online/refs/heads/distro/collabora/coda-$VERSION/qt/com.collabora.Office.metainfo.xml"
+export ICON="https://raw.githubusercontent.com/CollaboraOnline/online/refs/heads/distro/collabora/coda-$VERSION/qt/hicolor/scalable/apps/com.collaboraoffice.Office.startcenter.svg"
+export DESKTOP="https://raw.githubusercontent.com/CollaboraOnline/online/refs/heads/distro/collabora/coda-$VERSION/qt/com.collaboraoffice.Office.desktop"
+export META="https://raw.githubusercontent.com/CollaboraOnline/online/refs/heads/distro/collabora/coda-$VERSION/qt/com.collaboraoffice.Office.metainfo.xml"
 export OUTNAME=CODA-"$VERSION"-anylinux-"$ARCH".AppImage
 export ANYLINUX_LIB=1
 export OPTIMIZE_LAUNCH=1
