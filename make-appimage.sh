58afd1de2b9172f7b1661440b79b600565b38833
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2026-04-03 10:43:38 -0400

    simplify

--- make-appimage.sh
+++ make-appimage.sh
@@ -3,33 +3,27 @@
 set -eu
 
 ARCH=$(uname -m)
-VERSION="$(cat ~/version)"
-CODA_PATH="$(cat ~/CODA_PATH)"
-CORE_PATH="$(cat ~/CORE_PATH)"
+VERSION=$(pacman -Q collabora-office | awk '{print $2; exit}') # example command to get version of application here
 export ARCH VERSION
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.bg.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
-export ICON="https://raw.githubusercontent.com/CollaboraOnline/online/refs/heads/distro/collabora/coda-$VERSION/qt/hicolor/scalable/apps/com.collaboraoffice.Office.startcenter.svg"
-export DESKTOP="https://raw.githubusercontent.com/CollaboraOnline/online/refs/heads/distro/collabora/coda-$VERSION/qt/com.collaboraoffice.Office.desktop"
-export META="https://raw.githubusercontent.com/CollaboraOnline/online/refs/heads/distro/collabora/coda-$VERSION/qt/com.collaboraoffice.Office.metainfo.xml"
-export OUTNAME=CODA-"$VERSION"-anylinux-"$ARCH".AppImage
-export ANYLINUX_LIB=1
+export ICON=/usr/share/icons/hicolor/256x256/apps/com.collaboraoffice.Office.startcenter.png
+export DESKTOP=/usr/share/applications/com.collaboraoffice.Office.desktop
 export OPTIMIZE_LAUNCH=1
-export LOCOREPATH="$CORE_PATH"
-export PATH_MAPPING_HARDCODED=coda-qt
 
 # Deploy dependencies
-quick-sharun /usr/bin/coda-qt \
-    /usr/share/coda-qt \
-    /usr/share/coolwsd \
-    /usr/lib/core
+quick-sharun \
+	/usr/bin/coda-qt          \
+	/usr/lib/collabora-office \
+	/usr/share/coda-qt        \
+	/usr/share/coolwsd
 
-
-echo 'LOCOREPATH=${SHARUN_DIR}/lib/core' >> ./AppDir/.env
-echo 'COOL_TOPSRCDIR=${SHARUN_DIR}/share/coolwsd' >> ./AppDir/.env
-echo 'LC_CTYPE=en_US.UTF-8' >> ./AppDir/.env
-
+# Additional changes can be done in between here
 
 # Turn AppDir into AppImage
 quick-sharun --make-appimage
+
+# Test the app for 12 seconds, if the test fails due to the app
+# having issues running in the CI use --simple-test instead
+quick-sharun --test ./dist/*.AppImage
