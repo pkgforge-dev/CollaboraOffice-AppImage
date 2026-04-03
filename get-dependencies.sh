4e12276e8f35f1da2b0d224e07381ceb2f1171fa
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2026-04-03 16:33:54 -0400

    remove some bundled fonts

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -15,8 +15,12 @@ get-debloated-pkgs --add-common --prefer-nano
 # Comment this out if you need an AUR package
 make-aur-package collabora-office
 
-# remove dicts since this can use the system 	hunspell dicts instead
+# remove dicts since this can use the system hunspell dicts instead
 rm -rf /usr/lib/collabora-office/share/extensions
+
+# remove heavy bundled fonts since they can be used from the host
+d=/usr/lib/collabora-office/share/fonts/truetype
+rm -f "$d"/Noto* "$d"/LinLibertine*
 
 # If the application needs to be manually built that has to be done down here
 
